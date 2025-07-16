ce5b3cb861bbcf6a4046653aa17771774b1832cb
JoanieJay <joanehirim17@gmail.com>
2025-07-16 08:15:49 +0100

    Added Project

--- routes/bootcamp.js
+++ routes/bootcamp.js
@@ -6,24 +6,36 @@ const {
   updateBootcamp,
   deleteBootcamp,
   getBootcampsInRadius,
+  bootcampPhotoUpload,
 } = require("../controllers/bootcamp");
+const Bootcamp = require("../module/Bootcamp");
+const advancedResults = require("../middleware/advancedResults");
 
 // Include other resource routers
 const courseRouter = require("./courses");
 
 const router = express.Router();
 
+const { protect, authorize } = require("../middleware/auth");
+
 // Re-route into other resource routers
 router.use("/:bootcampId/courses", courseRouter);
 
 router.route("/radius/:zipcode/:distance").get(getBootcampsInRadius);
 
-router.route("/").get(getBootcamps).post(createBootcamp);
+router
+  .route("/:id/photo")
+  .put(protect, authorize("publisher", "admin"), bootcampPhotoUpload);
+
+router
+  .route("/")
+  .get(advancedResults(Bootcamp, "courses"), getBootcamps)
+  .post(protect, authorize("publisher", "admin"), createBootcamp);
 
 router
   .route("/:id")
   .get(getBootcamp)
-  .put(updateBootcamp)
-  .delete(deleteBootcamp);
+  .put(protect, authorize("publisher", "admin"), updateBootcamp)
+  .delete(protect, authorize("publisher", "admin"), deleteBootcamp);
 
 module.exports = router;
--- seeder.js
+++ seeder.js
@@ -9,6 +9,7 @@ dotenv.config({ path: "./config/config.env" });
 // Load Models
 const Bootcamp = require("./module/Bootcamp");
 const Course = require("./module/Course");
+const User = require("./module/User");
 
 // Connect to DB
 mongoose.connect(process.env.MONGO_URI);
@@ -20,12 +21,16 @@ const bootcamps = JSON.parse(
 const courses = JSON.parse(
   fs.readFileSync(`${__dirname}/_data/courses.json`, `utf-8`)
 );
+const users = JSON.parse(
+  fs.readFileSync(`${__dirname}/_data/users.json`, `utf-8`)
+);
 
 // Import into DB
 const importData = async () => {
   try {
     await Bootcamp.create(bootcamps);
-    // await Course.create(courses);
+    await Course.create(courses);
+    await User.create(users);
     console.log("Data Imported...".cyan.inverse);
     process.exit();
   } catch (err) {
@@ -38,6 +43,7 @@ const deleteData = async () => {
   try {
     await Bootcamp.deleteMany();
     await Course.deleteMany();
+    await User.deleteMany();
     console.log("Data deleted...".red.inverse);
     process.exit();
   } catch (err) {
